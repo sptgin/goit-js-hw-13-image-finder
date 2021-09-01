4d8b1b4ccaedd110a7898076c211e8c91964a9e8
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-09-01 15:48:32 +0300

    Добавляет вывод списка изображений
    Добавляет вывод списка изображений

--- src/index.js
+++ src/index.js
@@ -23,46 +23,36 @@ imageSearchInput.addEventListener('input', debounce(imageSearch, 1000));
 
 function imageSearch(event) {
   event.preventDefault();
-  imageSearchAPI.query = imageSearchInput.firstElementChild.value;
-  imageSearchAPI
-    .fetchImages()
-    .then(images => {
-      if (images.length !== 0) {
-        imageSearchListMake(images);
-      } else {
+  if (imageSearchInput.firstElementChild.value === '') {
+    imageSearchListClear();
+  } else {
+    imageSearchAPI.query = imageSearchInput.firstElementChild.value;
+    imageSearchAPI
+      .fetchImages()
+      .then(images => {
+        if (images.length !== 0) {
+          imageSearchListMake(images);
+        } else {
+          alert({
+            text: 'No matces found !',
+            delay: 1000,
+          });
+        }
+      })
+      .catch(() => {
         alert({
-          text: 'No matces found !',
+          text: 'No data for search ...',
           delay: 1000,
         });
-      }
-    })
-    .catch(() => {
-      alert({
-        text: 'No data for search ...',
-        delay: 1000,
       });
-    });
+  }
 }
 
 function imageSearchListMake(images) {
-  console.log('imageSearchListMake - START');
-  console.log(images);
-
-  // const imageSearchCardArray = images.map(imageCardTemplate).join(' ');
-
-  // console.log(imageSearchCardArray);
-
-  imageSearchCard.insertAdjacentHTML(
+  imageSearchList.insertAdjacentHTML(
     'beforeend',
     images.map(imageCardTemplate).map(imagesListTemplate).join(' '),
   );
-
-  // imageSearchList.insertAdjacentHTML(
-  //   'beforeend',
-  //   imageSearchCard.map(imagesListTemplate).join(' '),
-  // );
-  // console.log(imageSearchList);
-  console.log('imageSearchListMake - DONE');
 }
 
 function imageSearchListClear() {
